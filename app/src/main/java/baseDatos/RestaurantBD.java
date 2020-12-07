a075ff357a2aca4af5fbac65fd8f869860b9d878
Felipe <flype360360@gmail.com>
2020-12-07 16:21:46 -0600

    Se agrego la base Restaurant

---
+++ app/src/main/java/baseDatos/RestaurantBD.java
@@ -0,0 +1,28 @@
+package baseDatos;
+
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import Controladores.BaseDAO;
+import Entidades.Ordenes;
+
+@Database(entities = {Ordenes.class}, version = 1, exportSchema = false)
+public abstract class RestaurantBD extends RoomDatabase {
+
+    public abstract BaseDAO alumnoDao();
+    private static RestaurantBD INSTANCE;
+
+    public static RestaurantBD getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), RestaurantBD.class, "Restaurant").build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+
+}
