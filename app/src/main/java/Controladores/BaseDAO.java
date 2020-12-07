fc33e6fe1d07d4252cbd5b67ec5662941b9db8cd
Felipe <flype360360@gmail.com>
2020-12-07 15:42:59 -0600

    Se agregaron interfaz DAO para la base

--- app/src/main/java/Controladores/BaseDAO.java
+++ app/src/main/java/Controladores/BaseDAO.java
@@ -1,38 +1,16 @@
 package Controladores;
 
+
 import androidx.room.Dao;
-import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
-import androidx.room.Update;
 
 import java.util.List;
 
 import Entidades.Alumno;
 
 @Dao
-public interface AlumnoDAO {
-/*
-    @Insert
-    public void InsertarAlumnos(Alumno...Alumnos);
-
-    @Query("DELETE FROM alumno WHERE numControl = :nc")
-    void eliminarPorNumControl(String nc);
-
-    @Delete
-    void eliminar(Alumno alumno);
-
-    @Update
-    public void actualizarAlumno(Alumno a);
-
-    @Query("Select * FROM alumnos WHERE numControl LIKE :nc AND last_name LIKE :last")
-    Alumno findByName(String nc, String nombre);
-
-    @Query("SELECT * from Alumno")
-    List<Alumno> obtenerTodos();
-*/
-    //Altas
-
+public interface BaseDAO {
 
     @Insert
     public void insertarAlumno(Alumno alumno);
@@ -54,5 +32,6 @@ public interface AlumnoDAO {
 
     @Query("SELECT * FROM alumno WHERE numControl LIKE :nc")
     Alumno obtenerUno(String nc);
+
 
 }
