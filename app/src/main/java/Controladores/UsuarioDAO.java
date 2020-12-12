fc48ef4f19853fb1f96189bd30ffcd182b0100f5
Felipe <flype360360@gmail.com>
2020-12-12 17:49:04 -0600

    Creada una interfaz para altas de usuarios

--- app/src/main/java/Controladores/UsuarioDAO.java
+++ app/src/main/java/Controladores/UsuarioDAO.java
@@ -1,4 +1,19 @@
 package Controladores;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import Entidades.Ordenes;
+import Entidades.Usuario;
+
+@Dao
 public interface UsuarioDAO {
+
+    @Insert
+    public void insertarUsuario(Usuario usuarrio);
+
+
+    @Query("SELECT * FROM Usuario WHERE usuario LIKE :io")
+    Ordenes obtenerUno(String io);
 }
