a26b7f5194bacef05892f7e11076e2512a87f1b2
Felipe <flype360360@gmail.com>
2020-12-12 17:09:42 -0600

    Creada una base para los usuarios

--- app/src/main/java/baseDatos/UsuariosBD.java
+++ app/src/main/java/baseDatos/UsuariosBD.java
@@ -1,12 +1,35 @@
 package baseDatos;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 
-import Entidades.Alumno;
 
-@Database(entities = {Alumno.class}, version = 1, exportSchema = false)
-public class UsuariosBD {
+import Controladores.AlumnoDAO;
+import Entidades.Usuario;
 
-    
+@Database(entities = {Usuario.class}, version = 1, exportSchema = false)
+public abstract class UsuariosBD {
+
+
+    public abstract Usuario UsuarioDao();
+
+    private static EscuelaBD INSTANCE;
+
+    public static EscuelaBD getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), EscuelaBD.class, "escuela").build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+
+
+
+
 
 }
