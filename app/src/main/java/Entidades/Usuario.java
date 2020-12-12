86870d91cf69d1c3cf5c1335afe269f933b05c6c
Felipe <flype360360@gmail.com>
2020-12-12 17:32:53 -0600

    Creada una entidad para los usuarios

--- app/src/main/java/Entidades/Usuario.java
+++ app/src/main/java/Entidades/Usuario.java
@@ -1,4 +1,42 @@
 package Entidades;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Usuario {
+
+    @NonNull
+    @PrimaryKey
+    private String usuario;
+
+    @NonNull
+    @ColumnInfo(name ="contraseña")
+    private String contraseña;
+
+    public Usuario(@NonNull String usuario, @NonNull String contraseña) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    @NonNull
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(@NonNull String usuario) {
+        this.usuario = usuario;
+    }
+
+    @NonNull
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(@NonNull String contraseña) {
+        this.contraseña = contraseña;
+    }
+
 }
